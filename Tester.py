d11925f080f15c4a26c8758bd690fe8b0ac10621
r2dev2bb8 <ronak.badhe@gmail.com>
2020-03-23 14:37:59 -0700

    It's overfitting dataset

--- Tester.py
+++ Tester.py
@@ -40,9 +40,9 @@ def main(interactive = True):
             print(test(model, input("fen? ")))
     else:
         # data/testData/fen.txt, eval.txt
-        with open("data/unbiasfen.txt", 'r') as fin:
+        with open("data/testData/fen.txt", 'r') as fin:
             fens = [s[:-1] for s in fin.readlines()]
-        with open("data/unbiaseval.txt", 'r') as fin:
+        with open("data/testData/eval.txt", 'r') as fin:
             ev = [s[:-1] for s in fin.readlines()]
         evals = (evalSimplify(e) for e in ev)
         correct = 0
